23615e7df4e9127127bb2b051b16d1b0ee17ec37
Colin Larrue <colin.larrue@gmail.com>
2022-01-14 13:42:26 +0100

    Add comments router

--- src/api/router.py
+++ src/api/router.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from src.api.endpoints import characters
 from src.api.endpoints import episodes
+from src.api.endpoints import comments
 
 
 api_router = APIRouter()
 api_router.include_router(characters.router, prefix="/characters")
 api_router.include_router(episodes.router, prefix="/episodes")
+api_router.include_router(comments.router, prefix="/comments")
